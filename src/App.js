dda7f319420750d409c3c5d32a1bab91a25065fc
brochhay <vanchhaycute168@gmail.com>
2022-12-12 14:51:54 +0700

    change to jx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1> React is coolllllll</h1>
+      <h1> React is coolllllll 123455555</h1>
     </div>
   );
 }
